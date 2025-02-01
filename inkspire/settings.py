eef2cdaedde50964379025a14e213a76c9ed1922
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2025-02-01 15:51:46 +0000

    ADD keys

--- inkspire/settings.py
+++ inkspire/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r7sy9b)499d2pvf68_hw)bro8-%1@!gd1-$6@aevb%h6(q90as'
+#SECRET_KEY = 'django-insecure-r7sy9b)499d2pvf68_hw)bro8-%1@!gd1-$6@aevb%h6(q90as'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
