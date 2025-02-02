6b4bc00423235ad34e1a415ccf209f04536d96b0
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2025-02-02 00:52:07 +0000

    ADD Story model

--- writersarchive/models.py
+++ writersarchive/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Story(models.Model):
+    uid = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.CharField(max_length=200)
+    summary = models.CharField(max_length=200)
+    content = models.TextField()
+    dateposted = models.DateTimeField(auto_now_add=True)
+    #approved = models.IntegerField(choices=POSTSTATUS, default=0)
