bdb6bfab689f37e78800f830385f75e7f0967bf1
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2025-02-03 00:55:50 +0000

    ADD Comment model

--- writersarchive/models.py
+++ writersarchive/models.py
@@ -2,14 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 POSTSTATUS = ((0, "Draft"), (1, "Published"))
+RATING = ((0, "Not Rated"), (1, "★"), (2, "★★"), (3, "★★★"), (4, "★★★★"), (5, "★★★★★"))
 
 # Create your models here.
 class Story(models.Model):
-    uid = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stories")
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.CharField(max_length=200)
-    summary = models.CharField(max_length=200)
+    summary = models.TextField(max_length=500)
     content = models.TextField()
-    dateposted = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    posted_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=POSTSTATUS, default=0)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Story, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    rating = models.IntegerField(choices=RATING, default=0)
+    comment_text = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
