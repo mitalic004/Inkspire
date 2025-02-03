b53e5c436331295174567e93334924e551115a3e
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2025-02-03 19:26:49 +0000

    ADD Summernote

--- writersarchive/admin.py
+++ writersarchive/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Story
 from .models import Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Story)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Story)
 admin.site.register(Comment)
