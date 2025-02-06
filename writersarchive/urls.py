37cfa7c84f2c5da8af8c6dac52bd95ddbe240195
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2025-02-06 22:31:49 +0000

    ADD post_detail view

--- writersarchive/urls.py
+++ writersarchive/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.StoryList.as_view(), name='home'),
+    path('', views.StoryList.as_view(), name='home'),    
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
