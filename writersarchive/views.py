647e1158c9bb764d724383b1bf162d6ca16affd7
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2025-02-07 20:59:19 +0000

    UPDATE only viewing posts when signed in

--- writersarchive/views.py
+++ writersarchive/views.py
@@ -24,9 +24,16 @@ def post_detail(request, slug):
 
     queryset = Story.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "writersarchive/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )
