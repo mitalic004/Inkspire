956f188c82e67c24456a33d5eec75a6f3368e724
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2025-02-09 23:46:08 +0000

    UPDATE submission page

--- writersarchive/views.py
+++ writersarchive/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Story, Comment
 from .forms import CommentForm, SubmissionForm
+from slugify import slugify
 
 # Create your views here.
 class StoryList(generic.ListView):
@@ -28,10 +29,10 @@ def post_detail(request, slug):
 
     queryset = Story.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+    # Submit Comment
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -41,7 +42,7 @@ def post_detail(request, slug):
             comment.save()
             messages.add_message(
                 request, messages.SUCCESS,
-                'Comment submitted and awaiting approval'
+                'Comment submitted and awaiting approval.'
             )
 
     comment_form = CommentForm()
@@ -111,3 +112,36 @@ def submission_page(request):
             "submission_form": submission_form
         },
     )
+
+    queryset = Story.objects
+
+    if request.method == "POST":
+        submission_form = SubmissionForm(data=request.POST)
+        if submission_form.is_valid():
+            post = submission_form.save(commit=False)
+            post.author = request.user
+            post.status = 0
+            post.save()
+            
+            # Create post slug
+            testslug = slugify(post.author) + slugify(post.title)
+            if testslug in queryset:
+                slugnotunique = True
+                i = 0
+                while slugnotunique:
+                    tempslug = testslug
+                    tempslug += str(i)
+                    if tempslug in queryset:
+                        i += 1
+                    else:
+                        slugnotunique = False
+                        post.slug = tempslug
+                        break
+            else:
+                post.slug = tempslug
+                    
+
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Story submitted and awaiting approval.'
+            )
